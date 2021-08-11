3e6cfac4def5cb2d2f9b8b84c9745f5721a73301
kellycora <kellyc@evollve.co>
2021-08-11 11:46:54 -0500

    modales regitro y signIn

--- app/components/teacherMember/formSignUp.js
+++ app/components/teacherMember/formSignUp.js
@@ -1,53 +1,168 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button , Alert, Modal,  Pressable, } from 'react-native';
+
 
 export default function Register() {
+
   const [username, setName] = useState('')
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
+  const [modalVisible, setModalVisible] = useState(false);
 
-
-  function handleSubmit() {
+  function handleSubmit() {    
     console.log({ username, email, password })
-  }
+}
 
   return (
-    <View style={styles.container}>
-      <Text>Name</Text>
-      <TextInput
+    <View style={styles.centeredView}>
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          Alert.alert("Modal has been closed.");
+          setModalVisible(!modalVisible);
+        }}
+      >
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <Text style={styles.modalText}>Name</Text>
+            <TextInput
         placeholder="username"
         onChangeText={value => setName(value)}
         value={username}
       />
-       <Text>Email</Text>
-      <TextInput
+       <Text style={styles.modalText}>Email</Text>
+       <TextInput
         placeholder="example@example.com"
         onChangeText={value => setEmail(value)}
         value={email}
         keyboardType="email-address"
       />
-       <Text>Password</Text>
-      <TextInput
+      <Text style={styles.modalText}>Password</Text>
+       <TextInput
         placeholder="password"
         onChangeText={value => setPassword(value)}
         value={password}
         secureTextEntry
       />
-      <Button
+       <Button
         title="Create User"
         onPress={handleSubmit}
       />
+            <Pressable
+              style={[styles.button, styles.buttonClose]}
+              onPress={() => setModalVisible(!modalVisible)}
+            >
+              <Text style={styles.textStyle}>Close</Text>
+            </Pressable>
+          </View>
+        </View>
+      </Modal>
+      <Pressable
+        style={[styles.button, styles.buttonOpen]}
+        onPress={() => setModalVisible(true)}
+      >
+        <Text style={styles.textStyle}>Register</Text>
+      </Pressable>
     </View>
-  )
-}
+  );
+};
+
+const styles = StyleSheet.create({
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2
+  },
+  buttonOpen: {
+    backgroundColor: "#F194FF",
+  },
+  buttonClose: {
+    backgroundColor: "#2196F3",
+  },
+  textStyle: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center"
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: "center"
+  }
+});
+
+
+
+// export default function Register() {
+//   const [username, setName] = useState('')
+//   const [email, setEmail] = useState('')
+//   const [password, setPassword] = useState('')
+
+
+
+//   function handleSubmit() {
+//     console.log({ username, email, password })
+//   }
+
+//   return (
+//     <View style={styles.container}>
+//       <Text>Name</Text>
+//       <TextInput
+//         placeholder="username"
+//         onChangeText={value => setName(value)}
+//         value={username}
+//       />
+//        <Text>Email</Text>
+//       <TextInput
+//         placeholder="example@example.com"
+//         onChangeText={value => setEmail(value)}
+//         value={email}
+//         keyboardType="email-address"
+//       />
+//        <Text>Password</Text>
+//       <TextInput
+//         placeholder="password"
+//         onChangeText={value => setPassword(value)}
+//         value={password}
+//         secureTextEntry
+//       />
+//       <Button
+//         title="Create User"
+//         onPress={handleSubmit}
+//       />
+//     </View>
+//   )
+// }
 
   
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-  });
+//   const styles = StyleSheet.create({
+//     container: {
+//       flex: 1,
+//       backgroundColor: '#fff',
+//       alignItems: 'center',
+//       justifyContent: 'center',
+//     },
+//   });
   
