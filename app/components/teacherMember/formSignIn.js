1bc337bc7c8c1696c20698167a5266b80bd75d2b
kellyco2019 <48420443+kellyco2019@users.noreply.github.com>
2021-08-12 21:08:34 -0500

    singIn, signup, createLesson (#5)

--- app/components/teacherMember/formSignIn.js
+++ app/components/teacherMember/formSignIn.js
@@ -1,14 +1,68 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button , SafeAreaView , ActivityIndicator } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import axios from 'axios'
+import { useNavigation } from '@react-navigation/native';
 
 export default function SignIn() {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState(false)
+  const navigation = useNavigation()
 
   function handleSubmit() {
+
+    const storeData = async (value) => {
+      try {
+      await AsyncStorage.setItem('token', value)
+      const token =  await AsyncStorage.getItem('token')
+      console.log('token', token)
+     } catch (error) {
+        console.log(error)
+      }
+    }
+    
+    setLoading(true)
+    axios({
+      method: 'POST',
+      baseURL: 'http://192.168.20.21:8000',
+      url: '/teacher/signin',
+      data : { email, password },
+    })
+    .then(({ data }) => {
+      console.log(data)
+       //poner condicionalde saber que si tiene 
+       //token lo dirija a la parte de lessons sino no
+       // lo deje ir a la parte de lessons
+      if (data.token){
+        storeData(data.token)
+        navigation.navigate('Home', {params : data._id})
+      } else {
+        navigation.navigate('LogIn')
+      }})
+      .catch(() => {
+        setError(true)
+      })
+      .finally(() => {
+        setLoading(false)
+      })
     console.log({ email, password })
   }
-
+  if(loading) {
+    return (
+      <SafeAreaView style={styles.container}>
+        <ActivityIndicator size="large" />
+      </SafeAreaView>
+    )
+  }
+  if(error) {
+    return (
+      <SafeAreaView style={styles.container}>
+        <Text>oopp! can`t create user</Text>
+      </SafeAreaView>
+    )
+  }
   return (
     <View style={styles.container}>
        <Text>Email</Text>
@@ -27,7 +81,7 @@ export default function SignIn() {
       />
       <Button
         title="Sign In"
-        onPress={handleSubmit}
+        onPress={handleSubmit}  
       />
     </View>
   )
